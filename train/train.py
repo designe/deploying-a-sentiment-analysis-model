441fe678a0518d8413b1b998de31d894f6afe527
EC2 Default User <ec2-user@ip-172-16-27-41.us-west-2.compute.internal>
2020-09-09 09:51:21 +0000

    final version

--- train/train.py
+++ train/train.py
@@ -8,6 +8,7 @@ import pandas as pd
 import torch
 import torch.optim as optim
 import torch.utils.data
+import torch.nn as nn
 
 from model import LSTMClassifier
 
@@ -67,9 +68,23 @@ def train(model, train_loader, epochs, optimizer, loss_fn, device):
     device       - Where the model and data should be loaded (gpu or cpu).
     """
     
-    # TODO: Paste the train() method developed in the notebook here.
-
-    pass
+    for epoch in range(1, epochs + 1):
+        model.train()
+        total_loss = 0
+        for batch in train_loader:         
+            batch_X, batch_y = batch
+            batch_X = batch_X.to(device)
+            batch_y = batch_y.to(device)
+            
+            # TODO: Complete this train method to train the model provided.
+            model.zero_grad()
+            out = model(batch_X)
+            loss = loss_fn(out, batch_y)
+            loss.backward()
+            nn.utils.clip_grad_norm_(model.parameters(), 5)
+            optimizer.step()
+            total_loss += loss.data.item()
+        print("Epoch: {}, BCELoss: {}".format(epoch, total_loss / len(train_loader)))
 
 
 if __name__ == '__main__':
